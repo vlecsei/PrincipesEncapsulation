bf8d7ac0936f6c2414b2dcdc97ff53e5aabe4f74
vlecsei <vlecsei@gmail.com>
2018-09-18 15:15:29 +0200

    Version finale

--- src/com/vladimir/application/Principale.java
+++ src/com/vladimir/application/Principale.java
@@ -6,10 +6,14 @@ public class Principale {
 
     public static void main(String[] args) {
 	
-	Personne martin = new Personne("martin", "Java SARL");
-	Personne dupont = new Personne("dupont");
-	martin.afficher();
-	dupont.afficher();
+	Personne durand = new Personne ("durand", "j2e sa");    
+		durand.afficher();        
+		durand.quitterSociete();
+		durand.afficher();
+		durand.setSociete("ejb corporation");
+		durand.afficher();
+		durand.setSociete("ejb corporation");
+		durand.afficher();
 
     }
 
--- src/com/vladimir/entites/Personne.java
+++ src/com/vladimir/entites/Personne.java
@@ -10,35 +10,63 @@ public class Personne {
     
     
     
-    
     public String getNom() {
-        return nom;
+        return nom.toUpperCase();
     }
     public void setNom(String nom) {
         this.nom = nom;
     }
     public String getSociete() {
-        return societe;
+        return societe.toUpperCase();
     }
-    public void setSociete(String societe) {
-        this.societe = societe;
+    public void setSociete(String entreprise) {
+	if(societe != null && societe != "?") {
+	    System.out.println("Erreur :\n1-quitterSociete, puis\n2-affecterSociete");// Vérification si societe est déjà instanciée
+	    System.exit(1);
+	}else {
+	    this.societe = validerSociete(entreprise);
+	}
+        
     }
   
     public Personne(String nom) {
 	this.nom = nom;
     }
-    public Personne(String nom, String societe) {
+    public Personne(String nom, String entreprise) {
 	this.nom = nom;
-	this.societe = societe;
+	this.societe = validerSociete(entreprise);
     }
     public void afficher() {
-	if(getSociete() == null) {
-	    System.out.println( "Je m'appelle " + nom.toUpperCase() + " et je ne suis pas employé d'une entreprise.");
-	}else {
-	    System.out.println( "Je m'appelle " + nom.toUpperCase() + " et je travail chez " +societe.toUpperCase());
-	}
 	
+	if(societe == null || societe == PAS_DE_SOCIETE) {
+	    System.out.println( "Je m'appelle " + getNom() + " et je ne suis pas employé d'une entreprise.");
+	}else {
+	     System.out.println( "Je m'appelle " + getNom() + " et je travaille chez " +getSociete());
+	}
+    }
+	
+    
+    
+    public void quitterSociete() {
+	
+	if (societe == null || societe == PAS_DE_SOCIETE) {	    
+	    System.out.println("Je ne suis pas salarié : impossible de quitter la société");
+	    System.exit(1);
+	}else
+	    societe = PAS_DE_SOCIETE;		
     }
     
-   
+    
+    
+    private String validerSociete(String entreprise) {
+	
+	if (entreprise.length() > 30 || entreprise == "?") {
+	    System.out.println("Classe Personne, société incorrecte : ?");
+	    System.exit(2);
+	}
+	    return entreprise;
+    }
 }
+    
+   
+
